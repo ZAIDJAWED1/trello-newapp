7b6a6822a469ba5084287d33c97384dac2d3a54d
ZAIDJAWED1 <98639154+ZAIDJAWED1@users.noreply.github.com>
2023-06-14 18:37:39 +0500

    Delet the error effecting on Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,37 @@
 import React from 'react'
 import { IoIosArrowDown } from 'react-icons/io';
 import { TbBellRinging2Filled } from 'react-icons/tb';
+import { Link } from "react-router-dom";
+
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">
+      <Link className="navbar-brand" to="#">
         <h4 className="m-0">Trello</h4>
-      </a>
+      </Link>
       <ul className="navbar-nav me-auto mx-2 mb-2 mb-lg-0">
         <li></li>
         <li className="nav-item">
-          <a className="nav-link text-light" href="#">
+          <Link className="nav-link text-light" to="#">
             Workspace<IoIosArrowDown/>
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-light" href="#">
+          <Link className="nav-link text-light" to="#">
             Recent<IoIosArrowDown/>
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-light" href="#">
+          <Link className="nav-link text-light" to="#">
             Starred<IoIosArrowDown/>
-          </a>
+          </Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link text-light" href="#">
+          <Link className="nav-link text-light" to="#">
             More<IoIosArrowDown/>
-          </a>
+          </Link>
         </li>
         <li>
           <button className="btn btn-primary">+</button>
